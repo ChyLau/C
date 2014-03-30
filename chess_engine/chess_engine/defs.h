a9da55aa256819d7490b3ca911ba564b61dec026
Chy Lau <c.lau@student.tudelft.nl>
2014-03-30 09:34:07 +0200

    chess engine 1

---
+++ chess_engine/chess_engine/defs.h
@@ -0,0 +1,13 @@
+#ifndef DEFS_H_INCLUDED
+#define DEFS_H_INCLUDED
+
+typedef unsigned long long U64;
+
+#define NAME "Vice 1.0"
+#define BRD_SQ_NUM 120
+
+enum ( EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK);
+
+
+
+#endif // DEFS_H_INCLUDED
