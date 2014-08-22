060cc9bf375878938fab6a79d8e6e457c6ba0964
Chy Lau <chyho.lau@gmai.com>
2014-08-22 11:04:18 +0200

    ex28 part one

--- chardway/ex26/devpkg/db.c
+++ chardway/ex26/devpkg/db.c
@@ -8,10 +8,10 @@
 
 static FILE *DB_open(const char *path, const char *mode)
 {
-    return fopen(path, mode);
+    return fopen(path, mode);  //file, mode
 }
 
-static void DB_close(FILE *db)
+static void DB_close(FILE *db)  //takes in file pointer
 {
     fclose(db);
 }
@@ -21,10 +21,13 @@ static bstring DB_load()
     FILE *db = NULL;
     bstring data = NULL;
 
-    db = DB_open(DB_FILE, "r");
+    db = DB_open(DB_FILE, "r");  //read data from DB_FILE
     check(db, "Failed to read from db file: %s", DB_FILE);
 
-    DB_close(db);
+    data = bread((bNread)fread, db); //bread takes two arguments: pointer to function and stream pointer
+    check(data, "Failed to read from db file: %s", DB_FILE);
+
+    DB_close(db);  //closes the db pointer
     return data;
 
 error:
@@ -40,11 +43,11 @@ int DB_update(const char *url)
         log_info("Already recorded as installed: %s", url);
     }
 
-    FILE *db = DB_open(DB_FILE, "a+");
+    FILE *db = DB_open(DB_FILE, "a+");  //opens file for reading writing, appends if file exists
     check(db, "Failed to open DB file: %s", DB_file);
 
     bstring line = bfromcstr(url);
-    bconchar(line, '\n');
+    bconchar(line, '\n');  //adds \n to the end of 'line'
     int rc = fwrite(line->data, blength(line), 1, db);
     check(rc == 1, "Failed to append to the db.");
 
@@ -58,13 +61,13 @@ error:
 int DB_find(const char *url)
 {
     bstring data = NULL;
-    bstring line = bfromcstr(url);
+    bstring line = bfromcstr(url);  //convert url to other type
     int res = -1;
 
     data = DB_load();
     check(data, "Failed to load: %s", DB_FILE);
 
-    if(binstr(data, 0, line) == BSTR_ERR)
+    if(binstr(data, 0, line) == BSTR_ERR)  //search for line in data at position 0 and increasing; BSTR_ERR is equivalent to NULL
     {
         res = 0;
     }
@@ -82,11 +85,11 @@ error:
 
 int DB_init()
 {
-    apr_pool_t *p = NULL;
-    apr_pool_initialize();
-    apr_pool_create(&p, NULL);
+    apr_pool_t *p = NULL;  //allocates memory
+    apr_pool_initialize();  //sets up internal structures to use pools
+    apr_pool_create(&p, NULL);  //creates new pool
 
-    if(access(DB_DIR, W_OK | X_OK) == -1)
+    if(access(DB_DIR, W_OK | X_OK) == -1)  // checks if the file 'DB_DIR' can be accessed for writing and execute/search
     {
         apr_status_t rc = apr_dir_make_recursive(DB_DIR,
                 APR_UREAD | APR_UWRITE | APR_UEXECUTE |
@@ -114,7 +117,7 @@ int DB_list()
     bstring data = DB_load();
     check(data, "Failed to read load: %s", DB_FILE);
 
-    printf("%s", bdata(data));
+    printf("%s", bdata(data));  //prints data
     bdestroy(data);
     return 0;
 
