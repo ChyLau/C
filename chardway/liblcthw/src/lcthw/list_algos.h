0efe9a37ece1fdaf061dc4e2f4509cbde0c5ef6a
Chy Lau <chyho.lau@gmai.com>
2014-09-02 09:36:38 +0200

    ex33: not going to finish it

--- chardway/liblcthw/src/lcthw/list_algos.h
+++ chardway/liblcthw/src/lcthw/list_algos.h
@@ -1,33 +1,12 @@
-#ifndef lcthw_list_algos_h
-#define lcthw_list_algos_h
+#ifndef _lcthw_List_algos_h
+#define _lcthw_List_algos_h
 
-#include <stdlib.h>
+#include <lcthw/list.h>
 
-typedef int (*List_compare)(char *s1, char *s2);
+typedef int (*List_compare)(const void *a, const void *b);
 
-int sorted_order(char *s1, char *s2);
+int List_bubble_sort(List *list, List_compare cmp);
 
-struct ListNode;
-
-typedef struct ListNode {
-    struct ListNode *prev;
-    struct ListNode *next;
-    void *value;
-} ListNode;
-
-typedef struct List {
-    int count;
-    ListNode *first;
-    ListNode *last;
-} List;
-
-List *List_create();
-void List_push(List *list, void *value);
-int List_bubble_sort(List *list, (List_compare) strcmp);
-void List_destroy(List *list);
-
-#define LIST_FOREACH(L, S, M, V) ListNode *_node = NULL;\
-    ListNode *V = NULL;\
-    for(V = _node = L->S; _node != NULL; V = _node = _node->M)
+List *List_merge_sort(List *list, List_compare cmp);
 
 #endif
