39ff18228ffdd4c5b874cf0023040011a61fbbf4
Chy Lau <chyho.lau@gmai.com>
2014-09-02 09:47:33 +0200

    ex34: header file

---
+++ chardway/liblcthw/src/lcthw/darray.h
@@ -0,0 +1,31 @@
+#ifndef _darray_h
+#define _darray_h
+#include <stdlib.h>
+#include <assert.h>
+#include <lcthw/dbg.h>
+
+typedef struct DArray {
+    int end;
+    int max;
+    size_t element_size;
+    size_t expand_rate;
+    void **contents;
+} DArray;
+
+DArray *DArray_create(size_t element_size, size_t initial_max);
+
+void DArray_destroy(DArray *array);
+
+void DArray_clear(DArray *array);
+
+int DArray_expand(DArray *array);
+
+int DArray_contract(DArray *array);
+
+int DArray_push(DArray *array, void *el);
+
+void *DArray_pop(DArray *array);
+
+void DArray_clear_destroy(DArray *array);
+
+
