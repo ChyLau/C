c34c191af1dc8255d39e01c98b3762893b728469
Chy Lau <chyho.lau@gmai.com>
2014-09-18 00:04:34 +0200

    using getline() function now instead of fscanf or fgets, something is wrong with the find city function...

--- chardway/dijkstra/src/lcthw/list.c
+++ chardway/dijkstra/src/lcthw/list.c
@@ -24,7 +24,8 @@ static inline int City_find(List *list, char *name)
 {
     LIST_FOREACH(list, first, next, cur)
     {
-        if(cur->name == name)
+        log_info("cur name: %s", cur->name);
+        if(strcmp(cur->name, name) == 0)
         {
             return 0;
         }
@@ -39,8 +40,9 @@ void List_push(List *list, char *name)
     check_mem(city);
 
     city->name = name;
+    log_info("Push name: %s", city->name);
 
-    check(City_find(list, city->name) != 0, "City %s already exists.", city->name);
+    //check(City_find(list, city->name) != 0, "City %s already exists.", city->name);
 
     if(list->last == NULL)
     {
@@ -75,24 +77,25 @@ List *List_city(List *list)
     FILE *db = NULL;
     int i = 0;
     int num_cities = 0;
-    char city_name[MAX_LENGTH];
+    char *city_name = NULL;
+    size_t len = 0;
+    ssize_t read;
 
     db = db_open(DB_FILE, "r");
     check(db, "Failed to open database: %s", DB_FILE);
 
-    int rc = fscanf(db, "%d", &num_cities);
-    check(rc != 0, "Failed to read number of cities.");
+    check((read = getline(&city_name, &len, db)) != -1, "Failed to read line.");
+    num_cities = atoi(city_name);
     log_info("Number of cities: %d", num_cities);
-    log_info("File pointer position: %d", ftell(db));
 
     for(i = 0; i < num_cities; i++)
     {
-        fgets(city_name, MAX_LENGTH, db);
-        log_info("File pointer position: %d", ftell(db));
+        check((read = getline(&city_name, &len, db)) != -1, "Failed to read line.");
         log_info("City name: %s", city_name);
-
         List_push(list, city_name);
     }
+
+    free(city_name);
 
     db_close(db);
 
@@ -100,5 +103,7 @@ List *List_city(List *list)
 error:
     if(db)
         fclose(db);
+    if(city_name)
+        free(city_name);
     return NULL;
 }
